36d897d2a178786c0d47ffa787964c1ba0ebfa1f
Chris Cherry <ctcherry@gmail.com>
2011-08-11 17:37:51 -0700

    First commit, seems to work pretty well

---
+++ music.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+
+import os
+import socket
+import stat
+import sys
+
+from time import sleep
+
+class MusicList(object):
+    def __init__(self, file="~/.music"):
+        self.path = os.path.expanduser(file)
+        self.load()
+        
+    def exists(self):
+        return os.path.isfile(self.path)
+
+    def load(self):
+        if self.exists():
+            f = open(self.path)
+            music_lines = f.readlines()
+            f.close
+            music_lines.pop(0)
+            split_lines = [self._striplist(ml.split(':',1)) for ml in music_lines]
+            self.data = dict(split_lines)
+
+    def find(self, item):
+        if item in self.data:
+            return self.data[item]
+        else:
+            return None
+
+    def _striplist(self, l):
+        return([x.strip() for x in l])
+
+
+class VLCSocket(object):
+    def __init__(self, exec_path="/Applications/VLC.app/Contents/MacOS/VLC", socket_path="/tmp/vlc.sock"):
+        self.exec_path = exec_path
+        self.socket_path = socket_path
+        self.socket = None
+        
+    def connect(self):
+        if self.socket is None:
+            if not self.socket_exist():
+                os.system("%s --daemon -I oldrc --rc-unix=%s --rc-fake-tty" % (self.exec_path, self.socket_path))
+                sleep(1)
+                
+            self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            self.socket.connect(self.socket_path)
+        
+    def socket_exist(self):
+        if os.path.exists(self.socket_path):
+            mode = os.stat(self.socket_path).st_mode
+            if stat.S_ISSOCK(mode):
+                return True
+            else:
+                return False
+        return False
+    
+    def send(self, data):
+        if self.socket_exist():
+            if self.socket is not None:
+                self.socket.send(data+"\n")
+        
+    def close(self):
+        self.socket.close()
+        
+    def quit(self):
+        if self.socket_exist():
+            self.send('quit')
+    
+    def play(self, url):
+        self.connect()
+        self.send('clear')
+        self.send('add ' + url)
+        
+    def stop(self):
+        self.send('stop')
+            
+    def resume(self):
+        self.connect()
+        self.send('play')
+
+
+def main():
+    cmd_name = os.path.basename(sys.argv[0])
+    ml = MusicList()
+
+    if not ml.exists():
+        print "No Music file"
+        exit()
+    
+    if len(sys.argv) == 1:
+        print "Available streams:"
+        for name, url in ml.data.items():
+          print "%s (%s)" % (name, url)
+        print ""
+        print "- To play the first stream: %s %s" % (cmd_name, ml.data.keys()[0])
+        print "- To stop: %s stop" % (cmd_name)
+        print "- To resume: %s resume" % (cmd_name)
+        print "- To quit: %s quit" % (cmd_name)
+        print ""
+        exit()
+
+    vlc = VLCSocket()
+
+    if sys.argv[1] == 'quit':
+        vlc.quit()
+        exit()
+
+    if sys.argv[1] == 'stop':
+        vlc.stop()
+        exit()
+
+    if sys.argv[1] == 'resume':
+        vlc.resume()
+        exit()
+
+    url = ml.find(sys.argv[1])
+
+    if url is not None:
+        vlc.play(url)
+        exit()
+
+
+if __name__ == "__main__":
+    main()
